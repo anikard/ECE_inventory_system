ba9e95f613e71e9c5f4af9e0fe305a882b9ffff5
Anika RD <anika.radiya@gmail.com>
2017-01-28 16:19:03 -0500

    added modal for viewing single order with admin response form

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -49,13 +49,13 @@ module.exports = (function() {
 		                console.log("Successfully added an order!");
 
 		                // decrement number of products in stock
-				        Product.update(
-				        	{ name: req.body.product },
-							{ $inc: { num_left: -req.body.quantity } },
-							function(err, numberAffected, rawResponse) {
-		   							console.log('error', err);
-		   							}
-						)
+				  //       Product.update(
+				  //       	{ name: req.body.product },
+						// 	{ $inc: { num_left: -req.body.quantity } },
+						// 	function(err, numberAffected, rawResponse) {
+		   	// 						console.log('error', err);
+		   	// 						}
+						// )
 
 				    	res.end(); //  end the function to return
 		            }
@@ -82,13 +82,13 @@ module.exports = (function() {
 				    console.log("Successfully removed an order!");
 
 					// increment number of products in stock
-				        Product.update(
-				        	{ name: req.body.product },
-							{ $inc: { num_left: req.body.quantity } },
-							function(err, numberAffected, rawResponse) {
-		   							console.log('error', err);
-		   							}
-						)
+				  //       Product.update(
+				  //       	{ name: req.body.product },
+						// 	{ $inc: { num_left: req.body.quantity } },
+						// 	function(err, numberAffected, rawResponse) {
+		   	// 						console.log('error', err);
+		   	// 						}
+						// )
 
 				    res.end(); //  end the function to return
 			    }
