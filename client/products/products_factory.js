b14a320d7ce79881306439b82b9a8262c251a9de
Kevin Delgado <delgado1kevin@gmail.com>
2017-02-07 00:53:40 -0500

    authorization ifs added

--- client/products/products_factory.js
+++ client/products/products_factory.js
@@ -103,7 +103,23 @@ products_app.factory('ProductsFactory', function($http) {
 });
 
 
-products_app.controller('productsController', function($scope, ProductsFactory, $document) {
+products_app.controller('productsController', function($scope, auth, ProductsFactory, $document) {
+    $scope.authorized = false;
+    $scope.myName = auth.currentUser();
+    console.log($scope.myName);
+
+    $scope.logout = function() {
+      console.log("scope logging out ");
+      auth.logout(function() {
+        $scope.isLoggedIn = false;
+        window.location.assign("/");
+      });
+    }
+
+    $scope.isAuthorized = function() {
+      return false;
+    }
+
     $scope.products = ProductsFactory.getproducts(function(data) {
     $scope.products = data;
 
@@ -348,3 +364,54 @@ products_app.controller('tagsController', function($scope, ProductsFactory) {
   console.log("tags")
   console.log($scope.tags);
 })
+
+products_app.factory('auth', ['$http', '$window', function($http, $window){
+   var auth = {};
+
+    auth.getToken = function (){
+      return $window.localStorage['inventoryToken'];
+    }
+
+    auth.isLoggedIn = function(){
+      var token = auth.getToken();
+      if(token){
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return payload.exp > Date.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+
+    auth.currentUser = function(){
+      if(auth.isLoggedIn()){
+        var token = auth.getToken();
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return payload.username;
+      }
+    };
+
+    auth.logout = function(callback){
+      $window.localStorage.removeItem('inventoryToken');
+      callback();
+    };
+
+  return auth;
+}])
+/*
+products_app.controller('authController', function($scope, auth) {
+    $scope.myName = auth.currentUser();
+    console.log($scope.myName);
+
+    $scope.logout = function() {
+      console.log("scope logging out ");
+      auth.logout(function() {
+        $scope.isLoggedIn = false;
+        window.location.assign("/");
+      });
+    }
+
+    $scope.isAuthorized = function() {
+      return true;
+    }
+})
+*/
