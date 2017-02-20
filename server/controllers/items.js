39799a1e67665c199863b89036d46316781f2dca
ym67@duke.edu <ym67@duke.edu>
2017-02-19 22:40:59 -0500

    Lots of changes

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -22,23 +22,15 @@ module.exports = (app) => {
       if (item) {
         res.status(405).send({ error: "Item already exist!" });
       }
-      Field.find({}, 'name', function (err, results) {
-        if (err) return res.status(500).send({ error: err });
-        var fields = new Array;
-        results.forEach(e => {fields.push(e.name)});
-        console.log(fields);
-        props = _.pick(req.body, ['name','quantity','model','description','tags','image']);
-        props.fields = _.pick(req.body, fields);
-        console.log(props);
-        item = new Item(props);
-        item.save(function(err){
-            if(err){
-              res.status(500).send({ error: err })
-            } else {
-              res.status(200).send("success"); //  end the function to return
-            }
-        });
-      })
+      props = _.pick(req.body, ['name','quantity','model','description','tags','image','fields']);
+      item = new Item(props);
+      item.save(function(err){
+          if(err){
+            res.status(500).send({ error: err })
+          } else {
+            res.status(200).send("success"); //  end the function to return
+          }
+      });
     });
   });
 
@@ -57,21 +49,25 @@ module.exports = (app) => {
 
   app.post('/api/v2/item/update', function(req, res, next) {
     if (! req.body._id) return res.status(400).send({ error: "Missing ref id" });
-    Field.find({}, 'name', function (err, results) {
-      if (err) return res.status(500).send({ error: err });
-      var fields = new Array;
-      results.forEach(e => {fields.push(e.name)});
-      props = _.pick(req.body, ['name','quantity','model','description','tags','image']);
-      props.fields = _.pick(req.body, fields);
-      
-      Item.findByIdAndUpdate(
-        req.body._id,
-        {$set: props},
-        { new: true },
-        function (err, item) {
-          if (err) console.log("Update item Error");
-          res.json(item);
-        });
+    props = _.pick(req.body, ['name','quantity','model','description','tags','image','fields']);
+    Item.findByIdAndUpdate(
+    req.body._id,
+    {$set: props},
+    { new: true },
+    function (err, item) {
+      if (err) res.status(500).send({ error: err });
+      else res.status(200).json(item);
     });
+
+
+    // Field.find({}, 'name', function (err, results) {
+    //   if (err) return res.status(500).send({ error: err });
+    //   var fields = new Array;
+    //   results.forEach(e => {fields.push(e.name)});
+      
+    //   props.fields = _.pick(req.body, fields);
+      
+      
+    // });
   });
 }
