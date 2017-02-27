db2ba57878ae1b4efd2c3a1bf12530a9fad117e2
ym67@duke.edu <ym67@duke.edu>
2017-02-26 21:16:31 -0500

    Remove HTTP on 8000, Complete user API features

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -14,7 +14,7 @@ module.exports = (app) => {
   app.use('/api/user/:id/info', util.requirePrivileged);
   app.use('/api/user/show', util.requirePrivileged); 
   app.use('/api/user/add', util.requirePrivileged);
-  app.use('/api/user/update', util.requirePrivileged);
+  app.use('/api/user/update', util.requireLogin);
   app.use('/api/user/del', util.requirePrivileged);
 
   // Get current user info 
@@ -40,7 +40,9 @@ module.exports = (app) => {
 
   // Get user info 
   app.get('/api/user/:id/info', function(req, res, next){
-  	User.findOne({ '_id': req.params.id }, function (err, user) {
+    let fields = 'name username netId email status active date';
+    if (req.user.status === "admin") fields = `${fields} apiKey`;
+  	User.findOne({ '_id': req.params.id },fields, function (err, user) {
   		if(err) {
   			res.status(500).send({ error: err });
   		} else {
@@ -50,7 +52,9 @@ module.exports = (app) => {
   });
 
   app.get('/api/user/show', function(req, res, next){
-  	User.find({}, function(err, results) {
+    let fields = 'name username netId email status active date';
+    if (req.user.status === "admin") fields = `${fields} apiKey`;
+  	User.find({}, fields, function(err, results) {
   		if(err) {
   			res.status(500).send({ error: err });
   		} else {
@@ -60,29 +64,49 @@ module.exports = (app) => {
   });
 
   app.post('/api/user/add', function(req, res, next){
-  	var user = new User({name: req.body.name, date: new Date(), active: 'true'});
+    if (!req.body.name || !req.body.username || !req.body.password)
+      return res.status(400).send("Missing name, username or password");
+    let info = _.pick(req.body,['name','username','netId','email','status','active']);
+    info.date = new Date();
+    info.active = info.active || true;
+    info.status = info.status || 'user';
+  	var user = new User(info);
+    user.setPassword(req.body.password);
   	user.save(function(err){
   		if(err){
   			res.status(500).send({ error: err });
   		} else {
-  			res.status(200).send("Successfully updated a user!");
+  			res.status(200).send("Successfully added a user!");
   		}  	});
   });
 
   app.post('/api/user/update', function(req, res, next){
-  	return res.status(405);
-  	var user = new User({name: req.body.name, date: new Date(), active: 'true'});
-  	user.save(function(err){
-  		if(err){
-  			res.status(500).send({ error: err });
-  		} else {
-  			res.status(200).send("Successfully updated a user!");
-  		}
-  	});
+    req.body._id = req.body._id || req.body.user || req.user._id;
+    if (req.body._id != req.user._id && req.user.status!="admin" && req.user.status!="manager")
+      return res.status(403).send("Unauthorized");
+    info = _.pick(req.body,['name','username','netId','email','active']);
+    if ((req.user.status==="admin" || req.user.status==="manager") && req.body.status)
+      info.status = req.body.status;
+    User.findOne({ '_id': req.body._id }, function (err, user) {
+      if (err) return res.status(500).send({ error: err });
+      _.assign(user, info);
+      if (req.body.password) user.setPassword(req.body.password);
+      user.save(function(err){
+        if(err){
+          res.status(500).send({ error: err });
+        } else {
+          res.status(200).send("Successfully updated a user!");
+        }
+      });
+    });
+  	
   });
 
   app.post('/api/user/del', function(req, res, next){
-  	User.findOneAndRemove({'_id': req.body.user}, 
+    req.body._id = req.body._id || req.body.user;
+    if (!req.body._id) return res.status(400).send("Missing _id or user");
+    if (req.body._id === req.user._id) return res.status(400).send("Cannot delete yourself");
+  	User.findOneAndRemove({'_id': req.body._id}, 
   		function(err){
   			if(err){
   				res.status(500).send({ error: err });
