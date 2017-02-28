d047456f2cb236db576778428e8c3fa4093d5b3c
Kevin Delgado <delgado1kevin@gmail.com>
2017-02-28 01:54:43 -0500

    sanity check on add to cart quantities

--- client/products/products_factory.js
+++ client/products/products_factory.js
@@ -391,7 +391,8 @@ products_app.controller('productsController', function($scope, $window, $rootSco
     //var customerSelected = $document[ 0 ].getElementById('customerList');
     //$scope.new_order.customer_name = customerSelected.options[customerSelected.selectedIndex].text;
     console.log($scope.new_order);
-    //console.log("Current user stuff: ");
+    console.log("Current user stuff: ");
+    var stopper = $scope.new_order.quantity;
     //console.log($scope.user);
 
     //console.log($scope.authorized);
@@ -400,15 +401,19 @@ products_app.controller('productsController', function($scope, $window, $rootSco
     //}
     $scope.new_order.item = $scope.new_order.itemId;
 
-    ProductsFactory.addOrder($scope.new_order, function(data) {
-      $scope.new_order = {};
-    })
-    /*
-    $scope.currentTags = [];
-    $('#requestModal').modal('hide');
-    $('#productModal').modal('hide');
-    */
-    window.location.assign("/dispProducts");
+    var stopper = $scope.new_order.quantity;
+    if (stopper > 0){
+
+      ProductsFactory.addOrder($scope.new_order, function(data) {
+        $scope.new_order = {};
+      })
+      /*
+      $scope.currentTags = [];
+      $('#requestModal').modal('hide');
+      $('#productModal').modal('hide');
+      */
+      window.location.assign("/dispProducts");
+    }
   }
 
 
