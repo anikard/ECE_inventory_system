fcd2f54158c4a8878e71888bbba789552c613fc8
Kevin Delgado <delgado1kevin@gmail.com>
2017-03-07 10:41:30 -0500

    cleaned up factories

--- client/cart/cart_factory.js
+++ client/cart/cart_factory.js
@@ -20,13 +20,6 @@ var orders_app = angular.module('cart_app', []);
         console.log("getting cartItems in cart");
         $http.get('/api/cart/show').success(function(output) {
           orders = output;
-
-          console.log(orders);
-          orders.forEach(function(elem) {
-            //elem["customer_name"] = elem["userId"].name;
-          })
-          console.log(orders);
-
           callback(orders);
         })
       }
@@ -43,18 +36,8 @@ var orders_app = angular.module('cart_app', []);
         })
       }
 
-
-
-      // factory.addOrder = function(info, callback) {
-      //   $http.post('/addOrder', info).success(function(output) {
-      //       orders = output;
-      //       callback(orders);
-      //   })
-      // }
-
       factory.addToCart = function(info, callback) {
         console.log("adding to cart in factory");
-        // $http.post('/addToCart', info).success(function(output) {
         $http.post('/api/cart/add', info).success(function(output) {
             orders = output;
             callback(orders);
@@ -63,14 +46,11 @@ var orders_app = angular.module('cart_app', []);
 
       factory.createRequest = function(info, callback) {
         $http.post('/api/request/add', info).success(function(output) {
-            // orders = output;
-            // callback(orders);
             console.log("created request in factory of cart")
         })
         $('#orderModal').modal('hide');
         $('#disburseModal').modal('hide');
       }
-
 
       factory.removeFromCart = function(order, callback) {
         $http.post('/api/cart/del', order).success(function(output) {
@@ -84,39 +64,25 @@ var orders_app = angular.module('cart_app', []);
 
     orders_app.controller('ordersController', function($scope, $http, OrderFactory, auth, $document) {
 
-        $scope.user = OrderFactory.getuser(function(data) {
-          $scope.user = data;
-          $scope.myName = data.username || data.netId || data.name;
-        })
+      // AUTH
+      $scope.user = OrderFactory.getuser(function(data) {
+        $scope.user = data;
+        $scope.myName = data.username || data.netId || data.name;
+        $scope.authorized = data.status == "admin" || data.status == "manager";
+        $scope.adminOnly = data.status == "admin";
+      })
 
-        //console.log("USER ID: " + auth.currentUserID());
-        $scope.orders = {};
-        //var thisId = {userId: auth.currentUserID()};
-        OrderFactory.getorders(/*thisId,*/ function(data) {
-          $scope.orders = data;
-          console.log($scope.orders);
-
+      $scope.logout = function() {
+        $http.get('/api/auth/logout').success(function(output) {
+          $scope.isLoggedIn = false;
+          window.location.assign("/");
         });
-        //console.log("scope cartItems");
+      }
+
+      $scope.orders = OrderFactory.getorders(function(data) {
+        $scope.orders = data;
         console.log($scope.orders);
-        //$scope.myName = auth.currentUser();
-        //console.log("MY NAME: " + $scope.myName);
-        //$scope.authorized = (auth.currentUserStatus()=="admin");
-
-
-        // AUTH
-
-        $scope.currentUser = OrderFactory.getuser(function(data) {
-          $scope.currentUser = data;
-          $scope.authorized = data.status == "admin"
-        });
-
-        $scope.logout = function() {
-          $http.get('/api/auth/logout').success(function(output) {
-            $scope.isLoggedIn = false;
-            window.location.assign("/");
-          });
-        }
+      });
 
       $scope.customers = OrderFactory.getcustomers(function(data) {
         $scope.customers = data;
@@ -125,11 +91,8 @@ var orders_app = angular.module('cart_app', []);
       })
 
       $scope.addToCart = function() {
-
         console.log("addToCart from cart controller scope");
-
         var itemSelected = $document[ 0 ].getElementById('productList');
-
         if (!itemSelected.value || !$scope.new_order || !$scope.new_order.quantity) {
             alert('Please fill out all the fields.');
             return;
@@ -152,15 +115,9 @@ var orders_app = angular.module('cart_app', []);
       }
 
       $scope.createRequest = function() {
-
         console.log("createRequest from cart controller scope");
-
-        // console.log($scope.this_request.reason);
-
         $scope.this_request.items = $scope.orders;
-
         console.log($scope.this_request);
-
         OrderFactory.createRequest($scope.this_request, function(data) {
           $scope.orders = {};
           $scope.this_request = {};
@@ -169,7 +126,6 @@ var orders_app = angular.module('cart_app', []);
 
       $scope.removeFromCart = function(order) {
           $('#orderModal').modal('hide');
-          //order.userId = auth.currentUserID();
           OrderFactory.removeFromCart(order, function(data) {
             for (var i =0; i < $scope.orders.length; i++)
             {  if ($scope.orders[i]._id === order._id) {
@@ -178,10 +134,7 @@ var orders_app = angular.module('cart_app', []);
                 }
             }
         });
-
       }
-
-
     })
 
     orders_app.controller('productsController', function($scope, OrderFactory) {
@@ -189,71 +142,3 @@ var orders_app = angular.module('cart_app', []);
         $scope.products = data;
       })
     })
-
-    orders_app.factory('auth', ['$http', '$window', function($http, $window){
-       var auth = {};
-
-        auth.getToken = function (){
-          return $window.localStorage['inventoryToken'];
-        }
-
-        auth.isLoggedIn = function(){
-          var token = auth.getToken();
-          if(token){
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-            return payload.exp > Date.now() / 1000;
-          } else {
-            return false;
-          }
-        };
-
-        auth.currentUser = function(){
-          if(auth.isLoggedIn()){
-            var token = auth.getToken();
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-            return payload.username;
-          }
-        };
-
-        auth.currentUserID = function(){
-          if(auth.isLoggedIn()){
-            var token = auth.getToken();
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-            return payload._id;
-          }
-        };
-
-        auth.currentUserStatus = function(){
-          if(auth.isLoggedIn()){
-            var token = auth.getToken();
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-            return payload.status;
-          }
-        };
-
-        auth.getUserStatus = function () {
-          var userId = "";
-          if(auth.isLoggedIn()){
-            var token = auth.getToken();
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-            userId = payload._id;
-          }
-
-          $http.post('/getUser', userId).success(function(output) {
-            console.log(output);
-            callback(output);
-          })
-
-        }
-
-
-        auth.logout = function(callback){
-          $window.localStorage.removeItem('inventoryToken');
-          callback();
-        };
-
-      return auth;
-    }])
