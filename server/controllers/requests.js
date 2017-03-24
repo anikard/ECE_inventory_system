7e5841425340b5f6509f62e31543c53d35a71690
ym67@duke.edu <ym67@duke.edu>
2017-03-24 00:42:05 -0400

    Email for request change

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -5,6 +5,7 @@ var User = mongoose.model('User');
 var Item = mongoose.model('Item');
 var Log = mongoose.model('Log');
 var util = require('./util.js');
+var mailer = require('./mailer.js');
 var _ = require('lodash');
 
 module.exports = (app) => {
@@ -119,11 +120,31 @@ function add (req, res) {
 	    		let arr = [];
 	    		request.items.forEach(i=>arr.push(i.item));
 
-			let quantity_arr = [];
-			request.items.forEach(i=>quantity_arr.push(i.quantity));
-				
-			let name_arr = [];
-			request.items.forEach(i=>name_arr.push(i.item.name));
+	    		mailer.send({
+	    			to: req.user.email,
+	    			subject: `${req.user.username} added a new request`,
+	    			text: JSON.stringify(request),
+	    		});
+
+    			User.find({ 'subscribed': 'subscribed' }, function (err, users) {
+    				if(err) return next(err);
+    				if(!users.length) return;
+    				let addresses = users[0].email;
+    				for (var i = 1; i < users.length; i++) {
+    					addresses = `${addresses},${users[i].email}`;
+    				}
+    				mailer.send({
+    					to: addresses,
+    					subject: `${req.user.username} added a new request`,
+    					text: JSON.stringify(request),
+    				});
+    			})
+
+				let quantity_arr = [];
+				request.items.forEach(i=>quantity_arr.push(i.quantity));
+					
+				let name_arr = [];
+				request.items.forEach(i=>name_arr.push(i.item.name));
 			
 	    		let log = new Log({
 					init_user: id,
@@ -181,11 +202,23 @@ function disburse (id, req, res) {
 			cart.save((err)=>{
 				if (err)
 					return res.status(500).send({ error: err });
+
+				User.findOne({_id:id}, (err, user)=>{
+					if (err) return next(err);
+					let options = {
+		    			to: user.email,
+		    			subject: `${req.user.username} received a new disbursement`,
+		    			text: JSON.stringify(request),
+		    		};
+		    		console.log(options);
+					mailer.send(options);
+				});
+
 	    		let arr = []
 	    		request.items.forEach(i=>arr.push(i.item));
 
-			let quantity_arr = []
-			request.items.forEach(i=>quantity_arr.push(i.quantity));
+				let quantity_arr = []
+				request.items.forEach(i=>quantity_arr.push(i.quantity));
 
 	    		let log = new Log({
 					init_user: req.user,
@@ -218,6 +251,11 @@ function close (req, res)  {
 		{ new: true },
 		function (err, request) {
 			if (err) console.log("Error");
+			mailer.send({
+    			to: req.user.email,
+    			subject: `Your request has been closed`,
+    			text: JSON.stringify(request),
+    		});
 			res.end();
 		}
 	);
@@ -229,7 +267,12 @@ function del (req, res) {
 			if (err) {
 				res.status(500).send({ error: err });
 			} else {
-    			res.status(200).send("Successfully deleted an order!");
+    			res.status(200).send("Successfully deleted a request!");
+    			mailer.send({
+	    			to: req.user.email,
+	    			subject: `Your request has been deleted`,
+	    			text: JSON.stringify(request),
+	    		});
 			}
 
 			res.end();
@@ -267,10 +310,16 @@ function update (req, res) {
 	    		request.items.forEach(i=>arr.push(i.item));
 
 				let quantity_arr = []
-			request.items.forEach(i=>quantity_arr.push(i.quantity));
+				request.items.forEach(i=>quantity_arr.push(i.quantity));
 
 				let name_arr = []
-			request.items.forEach(i=>name_arr.push(i.item.name));				
+				request.items.forEach(i=>name_arr.push(i.item.name));	
+
+				mailer.send({
+	    			to: req.user.email,
+	    			subject: `Your request has been approved`,
+	    			text: JSON.stringify(request),
+	    		});			
 				
 	    		let log = new Log({
 					init_user: req.user,
@@ -289,6 +338,11 @@ function update (req, res) {
 			_.assign(request,_.pick(req.body,['note','status','reason','user']));
 			request.save(function (err, request) {
 				if (err) return res.status(500).send({ error: err});
+				mailer.send({
+	    			to: req.user.email,
+	    			subject: `Your request has been updated`,
+	    			text: JSON.stringify(request),
+	    		});	
 				return res.status(200).json(request);
 			});
 		}
