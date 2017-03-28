739cf9bbbe8f49f5aea4fd6120644810af35b43d
ym67@duke.edu <ym67@duke.edu>
2017-03-28 15:35:42 -0400

    Email

--- server/controllers/schedules.js
+++ server/controllers/schedules.js
@@ -6,6 +6,7 @@ var User = mongoose.model('User');
 var Email = mongoose.model('Email');
 var Request = mongoose.model('Request');
 var dateFormat = require('dateformat');
+var async = require("async");
 
 
 //var date = new Date(2012, 11, 21, 5, 30, 0);
@@ -13,46 +14,53 @@ var dateFormat = require('dateformat');
 var s = schedule.scheduleJob('*/5 * * * * *', function(){
   var now = new Date();
   var date_now = dateFormat(now, "yyyy-mm-dd");
-  Request.find({status: "onLoan"})
-  .populate('items.item user')
-  .exec((err, results) => {
+  Email.find({ dates: { $exists: true, $not: {$size: 0} } })
+  .exec((err, emails) => {
     if(err) return console.log(err);
-    results.forEach(request => {
-      if(request.user && request.user.email){
-        let user = request.user;
-        Email.find({ dates: { $exists: true, $not: {$size: 0} } })
-        .exec((err, results) => {
-          if (err) return console.log(err);
-          results.forEach(e => {
-            dates = e.dates;
-            var new_dates = [];
-            for(let i = 0; i < dates.length; i++){
-              dates[i] = dates[i].trim();
-              if(dates[i] > date_now) {
-                new_dates.push(dates[i]);
-              } else {
-                let body = e.body;
-                body += "\n----------------------------------\nRequest Summary\n";
-                body += `User: ${user.name} (${user.username})\n`;
-                body += `Reason: ${request.reason}\n`;
-                body += `Note: ${request.note || 'N/A'}\n`;
-                body += `Status: ${request.status}\n`;
-                body += `Type: ${request.type}\n`;
-                body += `Date: ${request.date}\n\n`;
-                body += `Items:\n`;
-                request.items.forEach(i=>body+=`Name: ${i.item.name}\t\tQuantity: ${i.quantity}\n`);
-                mailer.send({
-                  to: user.email,
-                  subject: e.subject,
-                  text: body,
-                });
-              }
-            }
-            e.dates = new_dates;
-            e.save();
+    emails.forEach(email => {
+      var new_dates = [];
+      email.dates.forEach(date => {
+        date = date.trim();
+        if(date > date_now) {
+          new_dates.push(date);
+        } else {
+          send(email, (err) => {
+            if (err) console.log(err);
           });
-        });
-      }
+        }
+      });
+      email.dates = new_dates;
+      email.save();
     });
   });
 });
+
+function send(email, callback) {
+  Request.find({status: "onLoan"})
+  .populate('items.item user')
+  .exec((err, requests) => {
+    if(err) return console.log(err);
+    async.each(requests,
+      (request, cb) => {
+        let user = request.user;
+        if(user && user.email) {
+          let body = email.body;
+          body += "\n----------------------------------\nRequest Summary\n";
+          body += `User: ${user.name} (${user.username})\n`;
+          body += `Reason: ${request.reason}\n`;
+          body += `Note: ${request.note || 'N/A'}\n`;
+          body += `Status: ${request.status}\n`;
+          body += `Type: ${request.type}\n`;
+          body += `Date: ${request.date}\n\n`;
+          body += `Items:\n`;
+          request.items.forEach(i=>body+=`Name: ${i.item.name}\t\tQuantity: ${i.quantity}\n`);
+          mailer.send({
+            to: user.email,
+            subject: email.subject,
+            text: body,
+          }, cb);
+        }
+      },
+      callback);
+    });
+}
