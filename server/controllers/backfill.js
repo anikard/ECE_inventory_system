980d79f66b9a1fdd0b23eac47ec2943514f8e4e0
ym67@duke.edu <ym67@duke.edu>
2017-04-09 14:46:55 -0400

    File upload

--- server/controllers/backfill.js
+++ server/controllers/backfill.js
@@ -22,6 +22,7 @@ module.exports = {
       app.post('/api/backfill/approve', util.requireLogin, approve);
       app.post('/api/backfill/deny', util.requireLogin, deny);
       app.post('/api/backfill/fulfill', util.requireLogin, done);
+      app.post('/api/backfill/upload', util.requireLogin, upload);
 
     }
 }
@@ -148,6 +149,37 @@ function email(backfill, subject){
       text: body,
     });
   })
+
+}
+
+function upload(req, res, next) {
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
 
 }
 
