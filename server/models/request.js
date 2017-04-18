178888655920dfc2cdbb43eff020b1bfe105e5bb
Kevin Delgado <delgado1kevin@gmail.com>
2017-04-18 05:20:27 -0400

    Removed redundancy from model

--- server/models/request.js
+++ server/models/request.js
@@ -43,9 +43,6 @@ var RequestSchema = new mongoose.Schema({
   backfills:[{type:Schema.ObjectId, ref:'Backfill', autopopulate: true}],
   notes: [{type: String, default: ""}],
 
-  backfills:[{type:Schema.ObjectId, ref:'Backfill'}],
-  notes: [{type: String, default:""}],
-
   //status: {type: String, default:"outstanding", enum: ['outstanding', 'onLoan', 'disbursed', 'returned', 'closed', 'approved', 'converted', 'denied']},
   type: {type: String, default: "disburse", enum: ['disburse', 'loan', 'backfill']},
   date: { type : Date, default: Date.now },
